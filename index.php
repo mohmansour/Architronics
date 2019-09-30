414149cde9757a4f3072836621940e40cdb50da1
mohamed mansour <mohamed.mnsoor_1995@yahoo.com>
2019-09-30 22:41:34 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<?php
-
-$mailFrom = $_POST['email'];
-$mailTo = $_POST["archi_tronics@yahoo.com"];
-$subject = $_POST['subject'];
-$mobile = $_POST['mobile'];
-$message =$_POST['message'];
-$msg= .$message "& the mobile phone is:" .$mobile;
-$headers = "Mail sent from: " .$mailFrom;
-mail($mailTo, $subject, $msg, $headers);
-
-?>
+<?php include 'test.html';?>
